54f27e5254f129ebb2db725aad57797819b9bc78
panama69 <github@flynnshome.com>
2016-08-25 15:55:35 -0700

    testing

--- remove_nga_folders.sh
+++ remove_nga_folders.sh
@@ -3,7 +3,9 @@ docker stop nga nga_es nga_oracle
 echo "removing containers"
 docker rm nga nga_es nga_oracle
 echo "removing images"
-docker rmi alexeiled/docker-oracle-xe-11g elasticsearch:2.2 hpsoftware/nga 
+docker rmi alexeiled/docker-oracle-xe-11g
+docker rmi elasticsearch:2.2 
+docker rmi hpsoftware/nga 
 rm -rf /etc/nga
 rm -rf /var/log/nga
 rm -rf /var/nga
