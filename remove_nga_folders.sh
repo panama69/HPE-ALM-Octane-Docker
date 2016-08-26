6a628e0ea62332b90442ea872f6fdd34f0e1038e
panama69 <github@flynnshome.com>
2016-08-26 14:32:41 -0500

    made more robust clean up
    stops and removes all containers that are pointed to the images for docker.  takes into account containers that are not using the standard names of nga nga_es nga_oracle so you can be sure to have a clean and working environment to start Octane.

--- remove_nga_folders.sh
+++ remove_nga_folders.sh
@@ -1,11 +1,58 @@
-echo "stopping containers"
-docker stop nga nga_es nga_oracle
-echo "removing containers"
-docker rm nga nga_es nga_oracle
-echo "removing images"
-docker rmi alexeiled/docker-oracle-xe-11g
-docker rmi elasticsearch:2.2 
-docker rmi hpsoftware/nga 
+#!/bin/bash
+
+images="elasticsearch:2.2|alexeiled/docker-oracle-xe-11g|hpsoftware/nga"
+containers=$(docker ps -a|grep -E $images|awk '{print $1}')
+
+echo "******************************"
+echo "******************************"
+echo "***  stopping containers"
+echo "******************************"
+echo "******************************"
+if [ -n "${containers}" ]; then
+     docker ps -a|grep -E $images|awk '{printf "%-20s %-12s %s\n",$NF,$1,$2}'
+     for c in $containers; do
+          docker stop $c >/dev/null
+     done
+else
+     echo "No matching containers to stop"
+fi
+
+echo
+echo
+echo "******************************"
+echo "******************************"
+echo "***  removing containers"
+echo "******************************"
+echo "******************************"
+if [ -n "${containers}" ]; then
+     docker ps -a|grep -E $images|awk '{printf "%-20s %-12s %s\n",$NF,$1,$2}'
+     for c in $containers; do
+          docker rm $c >/dev/null
+     done
+else
+     echo "No matching containers to remove"
+fi
+
+echo
+echo
+echo "******************************"
+echo "******************************"
+echo "***  removing images"
+echo "******************************"
+echo "******************************"
+list=$(echo $images|tr "|" "\n")
+for i in $list; do
+     echo $i
+     docker rmi $i >/dev/null
+done
+
+echo
+echo
+echo "******************************"
+echo "******************************"
+echo "***  removing mounted folders"
+echo "******************************"
+echo "******************************"
 rm -rf /etc/nga
 rm -rf /var/log/nga
 rm -rf /var/nga
